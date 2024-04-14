2751209883263702b362a30bcd4a06e0706b7ee2
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-04-14 00:22:04 -0400

    routing complete

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import NavBar from "../components/NavBar";
+
+function UserProfile() {
+
+    const params = useParams()
+    console.log(params)
+
+    const [user, setUser] = useState({});
+    const userId = params.id
+
+    useEffect(() =>{
+        fetch(`http://localhost:4000/users/${userId}`)
+        .then(r => r.json())
+        .then(data => setUser(data))
+        .catch(error => console.error(error))
+      }, [userId]);
+    
+  if(!user.name) {
+    return <h1>Loading ...</h1>
+  };
+
+  return(
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>{user.name}</h1>
+      </main>
+    </>
+  );
+};
+
+export default UserProfile;
